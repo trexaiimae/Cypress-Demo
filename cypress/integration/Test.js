00f7349fbde14b0287105c3c433f8cc415c70a40
trexaiimae <trexaiihurtado@gmail.com>
2025-08-30 16:56:28 +0800

    comit

--- cypress/integration/Test.js
+++ cypress/integration/Test.js
@@ -4,7 +4,10 @@ describe ('My First Test', () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
-    cy.get('.product:visbile').should('have.length',4);
+    cy.get('.product:visible').should('have.length',4);
 
+    //parent child chaining
+    cy.get('.products').find('.product').should('have.length',4);
+    
 })
 })
