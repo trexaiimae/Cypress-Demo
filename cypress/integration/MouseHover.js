edd596b7cf8daf3ed2561ac8e0f84c0bf894edb4
trexaiimae <trexaiihurtado@gmail.com>
2025-09-03 14:49:09 +0800

    handling hover elements

--- cypress/integration/MouseHover.js
+++ cypress/integration/MouseHover.js
@@ -3,6 +3,9 @@ describe ('Handling Web Tables', () => {
 
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 
+    // cy.get('div.mouse-hover-content').invoke('show')
+    cy.contains('Top').click({force:true}) //use only when not necessary to show the hover elements and just click on the element directly
+    cy.url().should('include','top')
 
 
 
